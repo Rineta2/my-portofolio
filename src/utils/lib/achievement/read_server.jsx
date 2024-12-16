a79437c306fd44a26e0a5ce6dc6ce1b637d14bc6
Rineta2 <rzkir571@gmail.com>
2024-12-17 06:29:25 +0700

    archivement

--- src/utils/lib/achievement/read_server.jsx
+++ src/utils/lib/achievement/read_server.jsx
@@ -1,19 +1,27 @@
 import { db } from "@/utils/firebase";
-import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
+
+import { collection, getDocs, query, orderBy } from "firebase/firestore";
 
 export const getAchievement = async () => {
-    return new Promise((resolve) => {
+    try {
         const q = query(
             collection(db, process.env.NEXT_PUBLIC_API_ACHIEVEMENT),
             orderBy("createdAt", "desc")
         );
 
-        const unsubscribe = onSnapshot(q, (snapshot) => {
-            const achievements = snapshot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data()
-            }));
-            resolve(achievements);
-        });
-    });
+        const achievementsSnap = await getDocs(q);
+        const achievements = achievementsSnap.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+            createdAt:
+                doc.data().createdAt?.toDate?.() ||
+                new Date(doc.data().createdAt) ||
+                new Date(),
+        }));
+
+        return achievements;
+    } catch (error) {
+        console.error("Error fetching achievements:", error);
+        return [];
+    }
 };
