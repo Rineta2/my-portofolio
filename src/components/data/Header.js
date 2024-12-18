e806e9357ea9d29e3f5ee6f985097555fdd7dfbb
Rineta2 <rzkir571@gmail.com>
2024-12-18 12:30:19 +0700

    footer

--- src/components/data/Header.js
+++ src/components/data/Header.js
@@ -1,3 +1,7 @@
+import { Facebook, Instagram, Linkedin, Youtube, Github } from "lucide-react";
+
+import { FaTiktok } from "react-icons/fa";
+
 export const logoName = [
   {
     name: "Rizki R",
@@ -28,5 +32,50 @@ export const navLink = [
     id: 4,
     name: "Contact",
     path: "/contact",
+  },
+];
+
+export const footer_copyright = [
+  {
+    id: 1,
+    name: "© 2024 by Rizki Ramadhan 👾",
+  },
+];
+
+export const footer_social = [
+  {
+    id: 1,
+    icons: <Facebook />,
+    path: "",
+  },
+
+  {
+    id: 2,
+    icons: <Instagram />,
+    path: "",
+  },
+
+  {
+    id: 3,
+    icons: <Linkedin />,
+    path: "",
+  },
+
+  {
+    id: 4,
+    icons: <Youtube />,
+    path: "",
+  },
+
+  {
+    id: 5,
+    icons: <Github />,
+    path: "",
+  },
+
+  {
+    id: 6,
+    icons: <FaTiktok />,
+    path: "",
   },
 ];
--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
 
+import styles from "@/components/layout/footer/footer.module.scss";
+
+import { footer_copyright, footer_social } from "@/components/data/Header"
+
+import Link from "next/link"
+
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className={styles.footer}>
+      <div className={`${styles.footer__container} ${styles.container}`}>
+        <h1>Ready to build?</h1>
+
+        <div className={`${styles.footer__social}`}>
+          {
+            footer_social.map((item, index) => {
+              return (
+                <Link key={index} href={item.path}>{item.icons}</Link>
+              )
+            })
+          }
+        </div>
+
+        {
+          footer_copyright.map((item, index) => {
+            return (
+              <p key={index}>{item.name}</p>
+            )
+          })
+        }
+      </div>
+    </footer>
   )
 }
