ca8181275834259b6ae1128a8f58e74e1ead3fdc
Rineta2 <rzkir571@gmail.com>
2024-12-30 03:05:29 +0700

    footer

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react'
 
 import styles from "@/components/layout/footer/footer.module.scss";
@@ -6,9 +8,14 @@ import { footer_copyright, footer_social } from "@/components/data/Header"
 
 import Link from "next/link"
 
+import { useTheme } from "@/utils/theme/ThemeContext";
+
 export default function Footer() {
+
+  const { isDarkMode } = useTheme();
+
   return (
-    <footer className={styles.footer}>
+    <footer className={`${styles.footer} ${isDarkMode ? styles.dark : styles.light}`}>
       <div className={`${styles.footer__container} ${styles.container}`}>
         <h1>Ready to build?</h1>
 
